8414581e4f15ab3ccf86b40019efdd646a78f954
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-09-17 00:15:55 -0600

    Update Appliance.java

--- Assignments/Lab0/src/appliance.ProblemDomain/Appliance.java
+++ Assignments/Lab0/src/appliance.ProblemDomain/Appliance.java
@@ -1,1 +1,58 @@
+package appliance.ProblemDomain;
+
+public abstract class Appliance {
+    protected String itemNumber;
+    protected String brand;
+    protected int quantity;
+    protected int wattage;
+    protected String color;
+    protected double price;
+
+    public Appliance(String[] data) {
+        this.itemNumber = data[0];
+        this.brand = data[1];
+        this.quantity = Integer.parseInt(data[2]);
+        this.wattage = Integer.parseInt(data[3]);
+        this.color = data[4];
+        this.price = Double.parseDouble(data[5]);
+    }
+
+    public abstract String toFileFormat();
+
+    public String getItemNumber() {
+        return itemNumber;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void decreaseQuantity() {
+        if (quantity > 0) {
+            quantity--;
+        }
+    }
+
+    public int getWattage() {
+        return wattage;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Item Number: " + itemNumber + "\nBrand: " + brand + "\nQuantity: " + quantity +
+               "\nWattage: " + wattage + "W\nColor: " + color + "\nPrice: $" + price;
+    }
+}
 
