bc2afd95af5ae6c32a51e1cdc71dcfa04831b96d
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-09-23 17:53:52 -0600

    Update NumberGuessingGame.java

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -1,1 +1,38 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        
+        int min = 1;
+        int max = 100;
+        int numberToGuess = random.nextInt(max - min + 1) + min; 
+        int numberOfTries = 0;
+        int guess;
+        boolean hasGuessedCorrectly = false;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("I'm thinking of a number between " + min + " and " + max + ". Can you guess it?");
+        
+        while (!hasGuessedCorrectly) {
+            System.out.print("Enter your guess: ");
+            guess = scanner.nextInt();
+            numberOfTries++;
+
+            if (guess < numberToGuess) {
+                System.out.println("Too low! Try again.");
+            } else if (guess > numberToGuess) {
+                System.out.println("Too high! Try again.");
+            } else {
+                hasGuessedCorrectly = true;
+                System.out.println("Congratulations! You guessed the number in " + numberOfTries + " tries.");
+            }
+        }
+
+        System.out.println("Thanks for playing!");
+        scanner.close();
+    }
+}
 
