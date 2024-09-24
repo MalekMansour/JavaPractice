1f87983b98c754be5e7a6e3cf558e8b5ec08c3c7
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-09-24 15:35:31 -0600

    Update TrapezoidArea.java

--- TrapezoidArea.java
+++ TrapezoidArea.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
+
+public class TrapezoidArea {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Height: ");
+        int height = scanner.nextInt();
+        
+        System.out.print("Base A: ");
+        int baseA = scanner.nextInt();
+        
+        System.out.print("Base B: ");
+        int baseB = scanner.nextInt();
+        
+        int bases = baseA + baseB;
+        int x = height * bases;
+        double area = x / 2.0;
+        
+        System.out.println("Area of the Trapezoid is " + area);
+        
+        scanner.close();
+    }
+}
 
