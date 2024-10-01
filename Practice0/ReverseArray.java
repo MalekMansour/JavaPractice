1bffe25b756f53dc21b1bd4b7fcf5eada4eca6f6
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-10-01 00:32:57 -0600

    Update ReverseArray.java

--- Practice0/ReverseArray.java
+++ Practice0/ReverseArray.java
@@ -1,1 +1,14 @@
+// Array Reversal Script
 
+function reverseArray(arr) {
+    return arr.reverse();
+}
+
+let originalArray = [1, 2, 3, 4, 5];
+
+console.log("Original array:", originalArray);
+
+let reversedArray = reverseArray(originalArray);
+
+console.log("Reversed array:", reversedArray);
+console.log("Original array after reversal:", originalArray);
